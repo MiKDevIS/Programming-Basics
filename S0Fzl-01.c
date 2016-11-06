a615ae5d7209fef2d7524325d3115beaa8e08c47
Milad Karimiyan <karimi@karimiyan.id.ir>
2016-11-07 01:05:56 +0330

    Create S0Fzl-01.c
    جواب سوال شماره ۱ از سری سوالات علی فاضل

---
+++ S0Fzl-01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Please enter 1st number??\t");
+    scanf ("%d", &a);
+    printf("Please enter 2nd number??\t");
+    scanf ("%d", &b);
+    printf("%d\n",a+b);
+    return 0;
+}
