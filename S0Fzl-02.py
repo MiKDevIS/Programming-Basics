77bc3403c198a0c7a9df431f894513ec37942cd8
H4midrezaBayat <hamidrezabayat76@outlook.com>
2016-11-08 21:51:02 +0330

    Add files via upload

---
+++ S0Fzl-02.py
@@ -0,0 +1,10 @@
+def main():
+    a = int(input("Please enter your number:"))
+    if a >= 0:
+        print("Positive")
+    else:
+        print("Negative")
+
+
+if __name__ == "__main__":
+    main()
